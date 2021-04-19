9982a042155e5a849bd89b2ed333a47cac8705e9
hi.jiang@ucloud.cn <hi.jiang@ucloud.cn>
2021-04-19 00:35:41 -0400

    添加rtmp文档

--- src/server/rtmp/rtmp_context.cpp
+++ src/server/rtmp/rtmp_context.cpp
@@ -4,26 +4,24 @@
 namespace mms {
 
 void RtmpServerContext::run() {
-    char c0c1[1537];
+    boost::array<char, 1537> c0c1;
     std::cout << "wait for c0, c1" << std::endl;
-    if(!tcp_socket_->recv(c0c1, 1537)) {
+    if(!tcp_socket_->recv(c0c1.data(), 1537)) {
         tcp_socket_->close(); // 关闭socket
         return;
     }
     std::cout << "recv c0 c1" << std::endl;
-    char *s0s1s2 = new char[3073];
-    _genS0S1S2(c0c1, s0s1s2);
+    boost::array<char, 3073> s0s1s2;
+    _genS0S1S2(c0c1.data(), s0s1s2.data());
     // send s0, s1, s2
-    if(!tcp_socket_->send(s0s1s2, 3073)) {
+    if(!tcp_socket_->send(s0s1s2.data(), 3073)) {
         tcp_socket_->close(); // 关闭socket
-        delete s0s1s2;
         return;
     }
 
-    char c2[1536];
-    if(!tcp_socket_->recv(c2, 1536)) {
+    boost::array<char, 1536> c2;
+    if(!tcp_socket_->recv(c2.data(), 1536)) {
         tcp_socket_->close(); // 关闭socket
-        delete s0s1s2;
         return;
     }
 
@@ -35,6 +33,7 @@ void RtmpServerContext::run() {
             tcp_socket_->close();
             return;
         }
+        // read basic header
         int32_t cid = (int32_t)(d & 0x3f);
         int8_t fmt = (d >> 6) & 0x03;
         if (cid > 1) {
@@ -57,7 +56,6 @@ void RtmpServerContext::run() {
             cid += (int32_t)(buf[1]);
         }
         // recv message header
-
         auto cid_it = chunk_streams_.find(cid);
         std::shared_ptr<RtmpChunk> chunk;
         if (cid_it == chunk_streams_.end()) {
@@ -66,6 +64,7 @@ void RtmpServerContext::run() {
         } else {
             chunk = cid_it->second;
         }
+
 
         std::cout << "cid:" << (uint32_t)cid << ", fmt:" << (uint32_t)fmt << std::endl;
         sleep(10000);
