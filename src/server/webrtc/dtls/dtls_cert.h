a45e6b1b59c72aa22a25b8df7d684dc632920dec
hi.jiang@ucloud.cn <hi.jiang@ucloud.cn>
2022-06-06 05:23:49 -0400

    优化生成cert证书代码

--- src/server/webrtc/dtls/dtls_cert.h
+++ src/server/webrtc/dtls/dtls_cert.h
@@ -26,20 +26,11 @@ namespace mms
             return finger_print_;
         }
         
-        SSL_CTX *getSSLCtx();
-
     private:
-        std::string sign_method_ = "sha256";
         X509 *certificate_ = nullptr;
         std::string finger_print_;
-        EVP_PKEY *private_key_ = nullptr;
-        SSL_CTX *ssl_ctx_ = nullptr;
         static DtlsCert instance_;
-
     private:
         bool createCert();
-        bool initSSLCtx();
-        static int sslVerifyCallback(int preverify_ok, X509_STORE_CTX *ctx);
-        static void sslInfo(const SSL *ssl, int where, int ret);
     };
 };
